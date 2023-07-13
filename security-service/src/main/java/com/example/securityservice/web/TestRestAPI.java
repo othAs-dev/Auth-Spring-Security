f36fcdea4f70f7a0b292d52d420dd64dc747aa1f
othAs-dev <othmane.developer@gmail.com>
2023-07-13 10:54:51 +0200

    creating JWT token

---
+++ security-service/src/main/java/com/example/securityservice/web/TestRestAPI.java
@@ -0,0 +1,19 @@
+package com.example.securityservice.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestRestAPI {
+    @GetMapping("/api/data-test")
+    public Map<String, Object> dataTest(Authentication authentication){
+        return Map.of(
+                "message", "Hello World",
+                "username", authentication.getName(),
+                "authorities", authentication.getAuthorities()
+        );
+    }
+}
